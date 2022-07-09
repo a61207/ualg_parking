e9cf67cc6f647f9c97170a2609b8c41b85d1cb15
a61207 <a61207@ualg.pt>
2022-07-09 17:22:29 +0100

    instalation bat

--- install.bat
+++ install.bat
@@ -1,3 +1,8 @@
-python -m venv venv | .\venv\Scripts\activate | tar -xf ualgParking\settings.zip -C ualgParking | pip3 install -r requirements.txt | python manage.py migrate |python manage.py loaddata inidb.json
+python -m venv venv
+.\venv\Scripts\activate
+tar -xf ualgParking\settings.zip -C ualgParking
+pip3 install -r requirements.txt
+python manage.py migrate
+python manage.py loaddata inidb.json
 
 pause

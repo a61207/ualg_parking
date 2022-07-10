faf8a1056a4aeb41a31724e4c4dfcc0ed5e6613d
a61207 <a61207@ualg.pt>
2022-07-10 02:36:32 +0100

    gitignore

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 tar -xf ualgParking\settings.zip -C ualgParking
 pip3 install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations main
+python manage.py migrate main
 python manage.py loaddata inidb.json
 
 pause

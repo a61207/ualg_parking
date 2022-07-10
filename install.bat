307de37aac9dd89f22cc1572d17130fb544918f2
a61207 <a61207@ualg.pt>
2022-07-10 12:41:28 +0100

    new data

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ tar -xf ualgParking\settings.zip -C ualgParking
 pip3 install -r requirements.txt
 python manage.py makemigrations main
 python manage.py migrate
-python manage.py loaddata inidb.json
+python manage.py loaddata popdb.json
 
 pause
